3a5dd4bc2b7896c17ddb759cabf4f983d7515bd0
silvallana <roxannesilvallana@gmail.com>
2024-07-03 11:16:30 +0800

    adding pokemons and styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const pokemonList = document.getElementById("#pokemonList");
+const pokemonList = document.getElementById("pokemon-list");
 
 let currentPokemons = [];
 
@@ -6,23 +6,37 @@ const limit = 10;
 let offset = 0;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
+function showPokemons() {
+    pokemonList.innerHTML = "";
+    currentPokemons.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const pokemonName = pokemon.name;
 
+        const pokemonItem = document.createElement("div");
+        pokemonItem.className = "pokemon-item";
+        pokemonItem.innerHTML = `
+        <h2 class="pokemon-info">${pokemonID} | ${pokemonName}</h2>
+        <div class="pokemon-image">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"/>
+        </div>
+        <p class="pokemon-type">This is the type</p>
+    `;
+    pokemonList.appendChild(pokemonItem);
+    });
+}
 
 function fetchPokemons() {
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        offset += limit;
-        currentPokemons = currentPokemons.concat(data.results);
-        displayPokemons(currentPokemons);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-}
-
-function displayPokemons() {
-    console.log(currentPokemons);
+        .then((response) => response.json())
+        .then((data) => {
+            offset += limit;
+            currentPokemons = currentPokemons.concat(data.results);
+            console.log(currentPokemons);
+            showPokemons();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
 
 fetchPokemons();
