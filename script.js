f49f73ee2b0e123ce630f164021f0b84b6ccf870
silvallana <roxannesilvallana@gmail.com>
2024-07-03 15:28:28 +0800

    fetch images using pokemonID + search/filter

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const pokemonList = document.getElementById("pokemon-list");
+const searchInput = document.getElementById("search-input");
+const numberFilter = document.getElementById("number");
+const nameFilter = document.getElementById("name");
+const notFoundMessage = document.getElementById("not-found-message");
+
 
 let currentPokemons = [];
 
@@ -14,14 +19,32 @@ function showPokemons() {
 
         const pokemonItem = document.createElement("div");
         pokemonItem.className = "pokemon-item";
-        pokemonItem.innerHTML = `
+        if (pokemonID < 10) {
+            pokemonItem.innerHTML = `
         <h2 class="pokemon-info">${pokemonID} | ${pokemonName}</h2>
         <div class="pokemon-image">
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"/>
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${pokemonID}.png"/>
         </div>
         <p class="pokemon-type">This is the type</p>
     `;
-    pokemonList.appendChild(pokemonItem);
+        } else if ((pokemonID > 10) || (pokemonID < 100)) {
+            pokemonItem.innerHTML = `
+        <h2 class="pokemon-info">${pokemonID} | ${pokemonName}</h2>
+        <div class="pokemon-image">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/0${pokemonID}.png"/>
+        </div>
+        <p class="pokemon-type">This is the type</p>
+    `;
+        } else {
+            pokemonItem.innerHTML = `
+        <h2 class="pokemon-info">${pokemonID} | ${pokemonName}</h2>
+        <div class="pokemon-image">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemonID}.png"/>
+        </div>
+        <p class="pokemon-type">This is the type</p>
+    `;
+        }
+        pokemonList.appendChild(pokemonItem);
     });
 }
 
@@ -32,11 +55,37 @@ function fetchPokemons() {
             offset += limit;
             currentPokemons = currentPokemons.concat(data.results);
             console.log(currentPokemons);
-            showPokemons();
+            showPokemons(currentPokemons);
         })
         .catch((error) => {
             console.log(error);
         })
 }
 
-fetchPokemons();
+function searchPokemon() {
+    const searchInput = searchInput.value.toLowerCase();
+    let filteredPokemons;
+
+    if (numberFilter.checked) {
+        filteredPokemons = currentPokemons.filter((pokemon) => {
+            const pokemonID = pokemon.url.split("/")[6];
+            return pokemonID.startsWith(searchInput);
+        });
+    } else if (nameFilter.checked) {
+        filteredPokemons = currentPokemons.filter((pokemon) =>
+            pokemon.name.toLowerCase().startsWith(searchInput)
+        );
+    } else {
+        filteredPokemons = currentPokemons;
+    }
+
+    showPokemons(filteredPokemons);
+
+    if (filteredPokemons.length === 0) {
+        notFoundMessage.style.display = "block";
+    } else {
+        notFoundMessage.style.display = "none";
+    }
+}
+
+fetchPokemons(currentPokemons);
